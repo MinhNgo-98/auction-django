8fb885e8b65e620406651252a4efdb24cfe58652
Minh <mh98ngo@gmail.com>
2021-03-08 02:31:24 +0100

    Update settings.py

--- commerce/settings.py
+++ commerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
+ALLOWED_HOSTS = ['127.0.0.1','venom-auction.herokuapp.com']
 
 
 # Application definition
